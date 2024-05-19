272bd8c57041a7f999f46ee19203c43583185ab6
Luis Primo Escandell <luisetepe6@gmail.com>
2024-05-19 21:25:01 +0200

    Removed unused file

--- test/test.utils.ts
+++
@@ -1,8 +0,0 @@
-import * as instrumentsSchema from '@db/schema/instrumentsTable'
-import { Database } from 'bun:sqlite'
-import { drizzle } from 'drizzle-orm/bun-sqlite'
-
-export function createTestDbContext(dbFilePath: string) {
-	const client = new Database(dbFilePath, { create: true })
-	return drizzle(client, { schema: { ...instrumentsSchema } })
-}
